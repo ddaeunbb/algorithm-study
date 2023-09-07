5f58647056fd6c178c132a896d6edbe80d2bae3f
Moonjonghoo <124956289+Moonjonghoo@users.noreply.github.com>
2023-09-07 18:08:17 +0900

    2023-09-07

---
+++ jonghoo/기본문제풀이/9.A를 #으로.js
@@ -0,0 +1,18 @@
+// 대문자로 이루어진 영어단어가 입력되면 단어에 포함된 ‘A'를 모두 ’#‘으로 바꾸어 출력하는
+// 프로그램을 작성하세요.
+
+function solution(string) {
+  let result = "";
+  if (string.toUpperCase() === string) {
+    for (x of string) {
+      if (x === "A") {
+        result += "#";
+      } else {
+        result += x;
+      }
+    }
+  }
+  console.log(result);
+}
+
+solution("BANANA");
