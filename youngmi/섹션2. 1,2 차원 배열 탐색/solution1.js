02d68ca50bce5e6ae67513f04358426100122a6a
suemeeeee <yungmi2909@gmail.com>
2023-09-11 13:31:02 +0900

    ✅ 09.11 문제풀이 (섹션2: 1, 2, 3)

---
+++ youngmi/섹션2. 1,2 차원 배열 탐색/solution1.js
@@ -0,0 +1,10 @@
+/* 큰 수 출력하기
+N(1<=N<=100)개의 정수를 입력받아, 자신의 바로 앞 수보다 큰 수만 출력하는 프로그램을 작
+성하세요.(첫 번째 수는 무조건 출력한다)
+ */
+
+const solution = (arr) => {
+  return arr.filter((num, idx) => idx === 0 || num > arr[idx - 1]);
+};
+
+console.log(solution([7, 3, 9, 5, 6, 12])); // [ 7, 9, 6, 12 ]
