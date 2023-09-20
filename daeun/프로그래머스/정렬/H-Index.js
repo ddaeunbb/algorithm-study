2783299ef3d6c9db863494ea2b36c64d749c62f7
ddaeunbb <ddaeunbb@gmail.com>
2023-09-20 15:17:33 +0900

    feat: 9월 20일 풀이

--- daeun/프로그래머스/정렬/H-Index.js
+++ daeun/프로그래머스/정렬/H-Index.js
@@ -16,10 +16,35 @@ function solution(citations) {
   return count;
 }
 
+// 통과된 내 풀이
 function solution(citations) {
-  citations.sort((a,b)=> b-a);
-  for (let i =0 ; i < citations.length; i++){
-      if( i >= citations[i]) return i
+  let max = citations.length;
+  let count = 0;
+  
+  // [25,8,5,3,3]
+  for(let i = 1; i <= max; i++){
+      for(let citation of citations){
+          if( citation >= i) count++;
+      }
+      if(count < i) return i-1;
+      else if(count === i) return i 
+      else count = 0;
   }
-  return citations.length;
+  return count;
+}
+
+
+// 영미님과 종후님 풀이
+function solution(citations){
+  let sortArr = citations.sort((a,b)=>b-a);
+  let answer = 0;
+
+  for(let i = 0; i < sortArr.length; i++){
+    if (sortArr[i] < i + 1) {
+      break;
+    } else {
+      answer++;
+    }
+  }
+  return answer;
 }
