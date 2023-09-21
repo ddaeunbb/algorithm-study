a59f22aa2daf072ee26c2e9973091d50a0a14428
ddaeunbb <ddaeunbb@gmail.com>
2023-09-21 16:39:48 +0900

    :memo: 완전탐색 3번까지 풀이이

--- daeun/문자열탐색/solution4.js
+++ daeun/문자열탐색/solution4.js
@@ -2,7 +2,7 @@
 // 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출 력하는 프로그램을 작성하세요.
 
 function solution(s, t){
-  const idx = [];
+  const idx = [1,5,10];
   const answer = [...s];
   const range = [-2,-1,1,2];
   
