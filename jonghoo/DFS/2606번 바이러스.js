a99c30864b2978636ed81d5a3eac99be7d45c581
Moonjonghoo <124956289+Moonjonghoo@users.noreply.github.com>
2023-10-16 20:47:16 +0900

    2023-10-16

---
+++ jonghoo/DFS/2606번 바이러스.js
@@ -0,0 +1,38 @@
+function solution(arr, n, m) {
+  let graph = [];
+  for (let i = 1; i <= n; i++) graph[i] = [];
+  for (let i = 0; i < m; i++) {
+    let [x, y] = arr[i].map(Number);
+    graph[x].push(y);
+    graph[y].push(x);
+  }
+  let cnt = 0;
+  let visited = new Array(n + 1).fill(false);
+  function dfs(x) {
+    visited[x] = true;
+    cnt++;
+    for (i of graph[x]) {
+      console.log(i);
+      if (!visited[i]) {
+        dfs(i);
+      }
+    }
+  }
+  dfs(1);
+  return cnt - 1;
+}
+
+console.log(
+  solution(
+    [
+      [1, 2],
+      [2, 3],
+      [1, 5],
+      [5, 2],
+      [5, 6],
+      [4, 7],
+    ],
+    7,
+    6
+  )
+);
