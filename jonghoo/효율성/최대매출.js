7eee580f5eed5a6d183f607bdd6c953e8d9819e9
Moonjonghoo <124956289+Moonjonghoo@users.noreply.github.com>
2023-10-24 21:11:59 +0900

    2023-10-24

--- jonghoo/효율성/최대매출.js
+++ jonghoo/효율성/최대매출.js
@@ -1,11 +1,20 @@
 function solution(N, K, arr) {
   let pri = N - K;
-  let left = 0;
-  let sum = 0;
   let result = [];
-  for (let i = 0; i < K; i++) {
-    sum += arr[left + i];
+
+  for (let i = 0; i <= pri; i++) {
+    let right = i; // right 변수 초기화
+    let sum = 0; // sum 변수 초기화
+
+    // right - i가 2 이하인 동안 sum에 값을 더합니다.
+    while (right - i <= 2 && right < N) {
+      sum += arr[right];
+      right++;
+    }
+
+    result.push(sum);
   }
+
   return result;
 }
 
