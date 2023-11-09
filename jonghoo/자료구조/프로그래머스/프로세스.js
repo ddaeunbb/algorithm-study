b67b8288fda49c12ef7177abe5f6e18e700590a2
Moonjonghoo <124956289+Moonjonghoo@users.noreply.github.com>
2023-11-09 10:59:31 +0900

    2023-11-09

---
+++ jonghoo/자료구조/프로그래머스/프로세스.js
@@ -0,0 +1,22 @@
+function solution(priorities, location) {
+  let answer = 0;
+  while (priorities.length > 0) {
+    const current = priorities.shift();
+    if (priorities.some((priority) => priority > current)) {
+      priorities.push(current);
+    } else {
+      answer++;
+      if (location === 0) {
+        break;
+      }
+    }
+    if (location > 0) {
+      location--;
+    } else {
+      location = priorities.length - 1;
+    }
+  }
+  return answer;
+}
+
+console.log(solution([2, 1, 3, 2], 1));
