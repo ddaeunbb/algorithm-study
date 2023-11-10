0831de5552e3972621e16d874e6a01c01ec2219a
ddaeunbb <ddaeunbb@gmail.com>
2023-11-10 14:22:58 +0900

    [feat]: 다리를 지나는트럭

---
+++ daeun/프로그래머스/스택,큐/다리를지나는트럭.js
@@ -0,0 +1,26 @@
+function solution(bridge_length, weight, truck_weights) {
+  let answer = 0;
+  let sum = 0;
+  const bridge = Array.from({length: bridge_length}, () => 0);
+  
+  // 초기셋팅 (1초일때)
+  answer++;
+  sum += truck_weights[0];
+  bridge.shift();
+  bridge.push(truck_weights.shift());
+  
+  while(sum > 0){
+    // 초 부터 더해주고
+    answer++;
+    sum -= bridge.shift();
+    if(bridge.length < bridge_length && sum + truck_weights[0] <= weight){
+      sum += truck_weights[0];
+      bridge.push(truck_weights.shift())
+    } else bridge.push(0);
+  }
+  return answer
+}
+
+console.log(solution(2, 10, [7,4,5,6])); //8
+console.log(solution(100, 100, [10])) // 101
+console.log(solution(100, 100, [10,10,10,10,10,10,10,10,10,10])) // 110
