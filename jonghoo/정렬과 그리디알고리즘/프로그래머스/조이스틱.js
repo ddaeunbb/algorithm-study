b89b7acb8ee4c1486bac32c0578b134749b21041
Moonjonghoo <124956289+Moonjonghoo@users.noreply.github.com>
2023-11-24 18:33:02 +0900

    test

--- jonghoo/정렬과 그리디알고리즘/프로그래머스/조이스틱.js
+++ jonghoo/정렬과 그리디알고리즘/프로그래머스/조이스틱.js
@@ -7,15 +7,28 @@ function movealphabat(s) {
 
 function solution(name) {
   let answer = 0;
+  let find = [];
   for (let i = 0; i < name.length; i++) {
-    if (name[i] == "A") {
-      answer++;
-    } else answer += movealphabat(name[i]);
+    find.push(movealphabat(name[i]));
   }
-  return answer;
+  console.log(find);
+  let minMove = find.length - 1;
+  for (let i = 0; i < find.length; i++) {
+    const temp = find[i];
+    answer += temp;
+
+    let cursor = i + 1;
+    while (cursor < find.length && find[cursor] === 0) cursor++;
+    console.log(cursor);
+
+    minMove = Math.min(
+      minMove,
+      i * 2 + find.length - cursor, //뒤돌기
+      i + 2 * (find.length - cursor) //뒤의요소입력하기
+    );
+    console.log(i * 2 + find.length - cursor, i + 2 * (find.length - cursor));
+  }
+  return answer + minMove;
 }
 
 console.log(solution("JAN"));
-console.log(movealphabat("O"));
-
-12 + 9;
