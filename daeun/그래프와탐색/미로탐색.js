0c7d3b5f6eff5e9e29d1e5a6b39db91626c97dbd
ddaeunbb <ddaeunbb@gmail.com>
2023-12-28 10:59:46 +0900

    [feat]: 미로탐색, BFS풀이

---
+++ daeun/그래프와탐색/미로탐색.js
@@ -0,0 +1,33 @@
+function solution(maze) {
+  let answer = 0;
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+
+  function DFS(x,y){
+    if(x===6 && y === 6) answer++;
+    else {
+      for(let k = 0; k<4; k++){
+        let nx = x + dx[k];
+        let ny = y + dy[k];
+        if(nx >= 0 && nx <= 6 && ny >= 0 && ny<= 6 && maze[nx][ny] === 0){
+          maze[nx][ny] = 1;
+          DFS(nx, ny);
+          maze[nx][ny] = 0;
+        }
+      }
+    }
+  }
+
+  maze[0][0] = 1;
+  DFS(0,0)
+  return answer;
+}
+
+console.log(solution([
+  [0,0,0,0,0,0,0],
+  [0,1,1,1,1,1,0],
+  [0,0,0,1,0,0,0],
+  [1,1,0,1,0,1,1,],
+  [1,1,0,0,0,0,1],
+  [1,1,0,1,1,0,0],
+  [1,0,0,0,0,0,0]])); // 8
