cf47561a3ef2f61b448cf749f7c78a398701a21b
ddaeunbb <ddaeunbb@gmail.com>
2023-12-29 16:05:56 +0900

    [feat]: 섬나라아일랜드, 송아지찾기 풀이

---
+++ daeun/그래프와탐색/송아지찾기.js
@@ -0,0 +1,53 @@
+// 앞으로 1, 뒤로 1, 앞으로 5
+function solution(s, e) {
+  let answer = 0;
+  let ch = Array.from({length: 10001}, () => 0);
+  let dis = Array.from({length: 10001}, () => 0);
+  let queue = [];
+  ch[s] = 1;
+  queue.push(s);
+  dis[s] = 0;
+
+  while(queue.length) {
+    let x = queue.shift();
+    for(let nx of [x-1, x+1, x+5]){
+      if(nx === e) return dis[x]+1;
+      if(nx > 0 && nx <= 10000 && ch[nx] === 0){
+        ch[nx] = 1;
+        queue.push(nx);
+        dis[nx] = dis[x] + 1;
+      }
+    }
+  }
+
+  return answer;
+}
+
+
+function solution(s, e){  
+  let answer=0;
+  let ch=Array.from({length:10001}, ()=>0);
+  let queue=[];
+  queue.push(s);
+  ch[s]=1;
+  let L=0;
+  while(queue.length){
+      let len=queue.length;
+      for(let i=0; i<len; i++){
+          let x=queue.shift();
+          for(let nx of [x-1, x+1, x+5]){
+              if(nx===e) return L+1;
+              if(nx>0 && nx<=10000 && ch[nx]===0){
+                  ch[nx]=1;
+                  queue.push(nx);
+              }
+          }
+      }
+      L++;
+  }
+  return answer;
+}
+
+
+console.log(solution(5, 14)); // 3
+solution(8, 3); // 5
