71ee924a7a7d21279e2c5e88e44d1b604442bdf3
ddaeunbb <ddaeunbb@gmail.com>
2024-01-03 14:11:29 +0900

    [feat]: 순위 풀이

--- daeun/프로그래머스/그래프/순위.js
+++ daeun/프로그래머스/그래프/순위.js
@@ -13,16 +13,17 @@
 //   })
 // }
 
-function BFS(graph, start) {
+function BFS(graph, start){
   const queue = [start];
+  let result = 0;
   const visited = [];
   visited[start] = true;
-  let result = 0;
 
-
+  // 큐가 빌 때까지, 노드를 계속 타고간다. 타면서 노드를 탄 개수를 카운트한다.
+  // 방문한 노드는 재방문하지 않는다.
   while(queue.length){
     const node = queue.shift();
-    for(let nextNode of (graph[node] || [])) {
+    for(nextNode of (graph[node] || [])){
       if(visited[nextNode] !== true){
         visited[nextNode] = true;
         queue.push(nextNode);
@@ -39,6 +40,9 @@ function solution(n, results) {
   const winGraph = [];
   const loseGraph = [];
 
+  // 이긴 그래프와, 진 그래프를 각각 만든다.
+  // 이긴 그래프 = [ _, [2], [5], [2], [2,3], _ ];
+  // 진 그래프 = [ _, _, [1, 3, 4], [4], _, [2] ];
   results.forEach(([a,b]) => {
     if(winGraph[a]) winGraph[a].push(b);
     else winGraph[a] = [b];
@@ -46,10 +50,9 @@ function solution(n, results) {
     else loseGraph[b] = [a];
   })
 
-  console.log(winGraph);
-  console.log(loseGraph);
+  // 이긴 그래프를 타고간 노드의 개수 + 진 그래프를 타고 간 노드의 개수 = n - 1 인 경우만 승리의 예측이 가능하다.
   for(let i = 1; i <= n; i++){
-    if(BFS(winGraph, i) + BFS(loseGraph, i) === n-1) answer++;
+    if(BFS(winGraph, i) + BFS(loseGraph, i) === n -1) answer++;
   }
 
   return answer;
